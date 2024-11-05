e44001f62b76132b534a9f92ca1b3e1b10963634
Marian Zajko <126076581+MarianZajko@users.noreply.github.com>
2024-11-05 14:19:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 20000
+    };
+
+    return member;
+}
